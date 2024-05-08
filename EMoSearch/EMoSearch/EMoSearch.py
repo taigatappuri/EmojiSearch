4a76393b5301a6813b3a78f036c38b27e0a0e78f
taigatappuri <2015taiga@gmail.com>
2024-05-08 23:46:31 +0900

    add: 入力場所の追加

--- EMoSearch/EMoSearch/EMoSearch.py
+++ EMoSearch/EMoSearch/EMoSearch.py
@@ -2,15 +2,53 @@ from rxconfig import config
 
 import reflex as rx
 
-class State(rx.State):
-    """The app state."""
+config.app_name = "EMoSearch"
 
+def SearchBox(words: str) -> rx.Component:
+    return rx.box(
+        words,
+        text_align="center",
+        padding=10,
+        margin=10,
+        border="1px solid black",
+        border_radius=5,
+        background_color="lightgrey",
+    )
+
+
+def search_part() -> rx.Component:
+    return rx.container(
+        rx.box(
+            "EMoSearch",
+            text_align="center",
+        ),
+        rx.box(
+            set_search_box(),
+        )
+    )
+
+def result_part() -> rx.Component:
+    return rx.container(
+        rx.box(
+            "検索結果",
+            text_align="center",
+        ),
+        rx.box(
+            "検索結果が表示されます",
+            text_align="center",
+        )
+    )
+
+def set_search_box() -> rx.Component:
+    return rx.hstack(
+        rx.input(placeholder="ワードを入力✨️"),
+        rx.button("検索🔍️"),
+    )
 
 def index() -> rx.Component:
-    return rx.center(
-        rx.box(
-            
-        )
+    return rx.container(
+        search_part(),
+        result_part(),
     )
 
 
