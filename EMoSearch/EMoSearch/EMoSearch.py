6f705a759ca5203bd4c9a0df4e99586bb7e5b671
taigatappuri <2015taiga@gmail.com>
2024-05-08 23:30:15 +0900

    clean: Refactor EMoSearch.py and test.ipynb

--- EMoSearch/EMoSearch/EMoSearch.py
+++ EMoSearch/EMoSearch/EMoSearch.py
@@ -1,12 +1,6 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
-
 from rxconfig import config
 
 import reflex as rx
-
-docs_url = "https://reflex.dev/docs/getting-started/introduction/"
-filename = f"{config.app_name}/{config.app_name}.py"
-
 
 class State(rx.State):
     """The app state."""
@@ -14,21 +8,9 @@ class State(rx.State):
 
 def index() -> rx.Component:
     return rx.center(
-        rx.theme_panel(),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text("Get started by editing ", rx.code(filename)),
-            rx.button(
-                "Check out our docs!",
-                on_click=lambda: rx.redirect(docs_url),
-                size="4",
-            ),
-            rx.logo(),
-            align="center",
-            spacing="7",
-            font_size="2em",
-        ),
-        height="100vh",
+        rx.box(
+            
+        )
     )
 
 
