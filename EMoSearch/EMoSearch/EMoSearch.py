aa6fe64874c1bd033a29f74f7d8951b23ee84cc5
taigatappuri <2015taiga@gmail.com>
2024-05-10 12:11:01 +0900

    fix: cardの調整。similarity を非表示に。

--- EMoSearch/EMoSearch/EMoSearch.py
+++ EMoSearch/EMoSearch/EMoSearch.py
@@ -7,39 +7,35 @@ import time
 from typing import List, Tuple
 
 def show_result(emoji: str, emoji_name: str, similarity: float) -> rx.Component:
-    return rx.box(
-        rx.hstack(
-            # カードのタイトル（絵文字とその名前）
-            rx.box(
-                rx.hstack(
-                    rx.box(
-                        rx.text(emoji, style={"font-size": "2em"}),
-                        text_align="center",    
-                    ),
-                    rx.box(
-                        rx.text(emoji_name, style={"font-size": "2em"}),
-                        text_align="center",
-                    ),
-                ),
-                style={"padding": "10px"},
+    return rx.flex(
+        # カードのタイトル（絵文字とその名前）
+        rx.box(
+            rx.text(emoji, style={"font-size": "2em"}),
+            text_align="center",    
+            margin_left="auto",
+        ),
+        rx.box(
+            rx.text(emoji_name, style={"font-size": "2em"}),
+            text_align="center",
+            margin_left="auto",
+        ),
+        # # similarity の表示
+        # rx.box(
+        #     rx.text(f"Similarity: {similarity:.2f}", style={"font-size": "1em"}),
+        #     text_align="center",
+        #     margin_left="auto",
+        #     style=style.similarity_style,
+        # ),
+        # コピーボタン
+        rx.box(
+            rx.button(
+                "コピー",
+                on_click=rx.set_clipboard(emoji),
+                style=style.button_style,
             ),
-            # similarity の表示
-            rx.box(
-                rx.text(f"Similarity: {similarity:.2f}", style={"font-size": "1em"}),
-                text_align="center",
-                style=style.similarity_style,
-            ),
-            # コピーボタン
-            rx.box(
-                rx.button(
-                    "コピー",
-                    on_click=rx.set_clipboard(emoji),
-                    style=style.button_style,
-                ),
-                text_align="center",
-                style={"padding": "10px"},
-            ),
-        ), 
+            text_align="center",
+            style={"padding": "10px"},
+        ),
         style=style.card_style,
         hover_style=style.card_hover_style,  # ホバー時のスタイル
     )
